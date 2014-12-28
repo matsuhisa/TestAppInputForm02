e29868d4ecfa2bac675135d1d2b4fd56c3fc34a1
Hironobu Matsuhisa <matsuhisa@gmail.com>
2014-12-28 16:27:52 +0900

    Initial Commit

---
+++ TestAppInputForm02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAppInputForm02
+//
+//  Created by 松久浩伸 on 2014/12/28.
+//  Copyright (c) 2014年 matsuhisa.hironobu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
