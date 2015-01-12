2a140f67a0d9e5dd515ee2f14524763772b00eb5
Hironobu Matsuhisa <matsuhisa@gmail.com>
2015-01-12 22:52:56 +0900

    delegateによる値の受け渡し

--- TestAppInputForm02/ThirdView.swift
+++ TestAppInputForm02/ThirdView.swift
@@ -3,17 +3,33 @@
 
 import UIKit
 
+@objc protocol ModalViewDelegate {
+    func saveText(textdata:String)
+}
+
 class ThirdViewController: UIViewController {
     
-
+    var delegate:ModalViewDelegate! = nil
+    
     @IBOutlet weak var ThisCloseButton: UIButton!
     @IBOutlet weak var AllCloseButton: UIButton!
+    @IBOutlet weak var InputTextThird: UIPlaceHolderTextView!
+    @IBOutlet weak var SaveButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // TextViewの設定
+        InputTextThird.placeHolder = "3枚目のViewです。ここに本文を書き、1枚目に送ります"
+        
+        // ボタンの設定
         ThisCloseButton.addTarget(self, action: "backToMain:", forControlEvents: .TouchUpInside)
         AllCloseButton.addTarget(self, action: "allClose:", forControlEvents: .TouchUpInside)
+        SaveButton.addTarget(self, action: "saveSubmit:", forControlEvents: .TouchUpInside)
+        
+        // rootViewからdelegate を設定する
+        var rootView  = UIApplication.sharedApplication().keyWindow.rootViewController as ViewController
+        self.delegate = rootView
     }
     
     override func didReceiveMemoryWarning() {
@@ -24,7 +40,16 @@ class ThirdViewController: UIViewController {
         self.dismissViewControllerAnimated(true, completion: nil);
     }
     
-    func allClose(sender: AnyObject?){
+    func allClose(sender: AnyObject?) {
         UIApplication.sharedApplication().keyWindow.rootViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func saveSubmit(sender: AnyObject?) {
+        
+        var inputText:String = InputTextThird.text
+        self.delegate.saveText(inputText)
+
+        // 閉じる
+        self.allClose(sender)
     }
 }
--- TestAppInputForm02/ViewController.swift
+++ TestAppInputForm02/ViewController.swift
@@ -3,33 +3,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ModalViewDelegate{
     
     @IBOutlet weak var TestWebView: UIWebView!
     @IBOutlet weak var ImutView: UIPlaceHolderTextView!
     @IBOutlet weak var SaveButton: UIButton!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // TextViewの設定
         ImutView.placeHolder = "本文です。"
         ImutView.placeHolderColor = UIColor(red: 255, green: 0, blue: 0, alpha: 0.5)
         
+        // ボタンの設定
         SaveButton.addTarget(self, action: "click:", forControlEvents: .TouchUpInside)
-        
-        // 文字列からWebViewを表示する
-        /*
-        var html:String = "<html><head></head><body><a href=\"http://www.yahoo.co.jp/\">Yahoo!</a></body></html>"
-        var htmldata = html.dataUsingEncoding(NSUTF8StringEncoding)
-        TestWebView.loadData(htmldata, MIMEType: "text/html", textEncodingName: "utf-8", baseURL: nil)
-        */
-        
-        // URLからWebViewを表示する
-        /*
-        var url: NSURL = NSURL.URLWithString("http://www.yahoo.co.jp/")
-        var urlRequest: NSURLRequest = NSURLRequest(URL: url)
-        TestWebView.loadRequest(urlRequest)
-        */
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,17 +26,17 @@ class ViewController: UIViewController {
     }
     
     func click(sender: AnyObject?) {
-        //var secondView:SecondViewController = SecondViewController()
-        //self.presentViewController(secondView, animated: true, completion:nil)
-        
-        //var secondView:AnyObject! = self.storyboard?.instantiateViewControllerWithIdentifier("second")
         // http://ushisantoasobu.hateblo.jp/entry/2014/09/12/023926
         var secondView:SecondViewController = self.storyboard!.instantiateViewControllerWithIdentifier("second") as SecondViewController
 
         secondView.modalPresentationStyle = UIModalPresentationStyle.OverFullScreen
 
-        
         self.presentViewController(secondView as UIViewController, animated: true, completion:nil)
+    }
+    
+    func saveText(textdata:String){
+        println("ViewController.textdata")
+        ImutView.text = textdata
     }
 }
 
