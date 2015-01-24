956c0548cafc83080179a260b60c0d4b4eb048ba
Hironobu Matsuhisa <matsuhisa@gmail.com>
2015-01-24 23:26:52 +0900

    UITextView にフォーカスがあったときの処理を追加

--- TestAppInputForm02/ViewController.swift
+++ TestAppInputForm02/ViewController.swift
@@ -3,11 +3,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, ModalViewDelegate{
+class ViewController: UIViewController, ModalViewDelegate, UITextFieldDelegate, UITextViewDelegate{
     
     @IBOutlet weak var TestWebView: UIWebView!
     @IBOutlet weak var ImutView: UIPlaceHolderTextView!
     @IBOutlet weak var SaveButton: UIButton!
+    @IBOutlet weak var InputTitle: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,14 +17,19 @@ class ViewController: UIViewController, ModalViewDelegate{
         ImutView.placeHolder = "本文です。"
         ImutView.placeHolderColor = UIColor(red: 255, green: 0, blue: 0, alpha: 0.5)
         
+        ImutView.delegate = self
+        
+        InputTitle.delegate = self
+        
         // ボタンの設定
-        SaveButton.addTarget(self, action: "click:", forControlEvents: .TouchUpInside)
+        //SaveButton.addTarget(self, action: "click:", forControlEvents: .TouchUpInside)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    // 2枚目のモーダルウィンドウ
     func click(sender: AnyObject?) {
         // http://ushisantoasobu.hateblo.jp/entry/2014/09/12/023926
         var secondView:SecondViewController = self.storyboard!.instantiateViewControllerWithIdentifier("second") as SecondViewController
@@ -33,6 +39,46 @@ class ViewController: UIViewController, ModalViewDelegate{
         self.presentViewController(secondView as UIViewController, animated: true, completion:nil)
     }
     
+    // 3枚目のモーダルウィンドウ
+    func clickToThird(sender: AnyObject?) {
+        var thirdView:AnyObject! = self.storyboard?.instantiateViewControllerWithIdentifier("third")
+        self.presentViewController(thirdView as UIViewController, animated: true, completion:nil)
+    }
+
+    // textViewが選択された時
+    func textViewDidBeginEditing(textView: UITextView) {
+        println("textViewDidBeginEditing -----------")
+        self.clickToThird(self)
+    }
+    
+    // textViewの選択が外れた時
+    func textViewDidEndEditing(textView: UITextView) {
+        println("textViewDidEndEditing -----------")
+    }
+    
+    // ？
+    func textViewShouldBeginEditing() ->Bool{
+        println("textViewShouldBeginEditing -----------")
+        return false
+    }
+    
+    /*
+    func textFieldShouldReturn(textField: UITextField!) -> Bool{
+        println( textField.text )
+        return true
+    }
+    
+    func textFieldShouldBeginEditing(textField: UITextView) -> Bool {
+        println("textFieldShouldBeginEditing ------")
+        return false
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextView) {
+        println("textFieldDidBeginEditing ------")
+    }
+    */
+    
+    // 保存処理
     func saveText(textdata:String){
         ImutView.text = textdata
     }
